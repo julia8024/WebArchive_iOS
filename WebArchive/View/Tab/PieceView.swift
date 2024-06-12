f5663b0d1d530e500b7d279360ca07022ece2436
julia8024 <julia8024@naver.com>
2024-06-13 01:53:10 +0900

    뷰 수정 : 탭바 - 조각 뷰에 조각 리스트 추가

--- WebArchive/View/Tab/PieceView.swift
+++ WebArchive/View/Tab/PieceView.swift
@@ -6,12 +6,45 @@
 //
 
 import SwiftUI
+import LinkPresentation
+import UniformTypeIdentifiers
+
 
 struct PieceView: View {
+    
+    @Environment(\.managedObjectContext) var manageObjContext
+    @FetchRequest(sortDescriptors: []) var link: FetchedResults<Link>
+    
+    let columns: [GridItem] = Array(repeating: GridItem(.flexible()), count: 4)
+    
+    let links: [StringLink] = [
+        StringLink(id: UUID(), url: "https://www.apple.com"),
+        StringLink(id: UUID(), url: "https://naver.com"),
+        StringLink(id: UUID(), url: "https://velog.io"),
+        StringLink(id: UUID(), url: "https://github.com"),
+        StringLink(id: UUID(), url: "https://school.programmers.co.kr/learn/challenges/beginner"),
+        StringLink(id: UUID(), url: "http://chat.openai.com/"),
+        StringLink(id: UUID(), url: "https://youtube.com"),
+        StringLink(id: UUID(), url: "https://solved.ac/")
+    ]
+    
+    @State private var inputTitle: String = ""
+    @State private var inputURL: String = ""
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+        // 그리드뷰
+        ScrollView {
+            
+            if (links.count > 0) {
+                PieceItemList(itemList: links)
+            }
+        }
     }
+
 }
+
 
 struct PieceView_Previews: PreviewProvider {
     static var previews: some View {
